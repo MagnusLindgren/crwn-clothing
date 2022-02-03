731ae1757db4a193357682bd0ad375530d2caf77
Magnus Lindgren <magnus.c.lindgren@gmail.com>
2022-02-03 15:49:51 +0100

    broken down homepage into smaller components and menuitem.component is finshed

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import './hompage.style.scss';
+
+import Directory from '../../components/directory/directory.component';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <Directory />
+    </div>
+);
+
+export default HomePage;
